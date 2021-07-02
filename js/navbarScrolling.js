8c3d544bb3564a6d0ce885a2e069b3ea18d70067
AndreChavs <keysandr7@gmail.com>
2021-07-01 21:43:39 -0300

    new project

--- js/navbarScrolling.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('scroll',function () {
-    let header = window.document.querySelector('header')
-    let windowPosition = window.scrollY > 0
-    header.classList.toggle('scrolling-active',windowPosition)
-})
